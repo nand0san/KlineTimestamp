0101254f865533394bc95b67f5ed2e144c58e9f2
root <root@DEEPLAB>
2024-10-20 20:34:44 +0200

    v0.1.6

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(requirements_path, encoding="utf-8") as f:
 
 setup(
     name="kline_timestamp",  # Nombre del paquete en minúsculas
-    version="0.1.5",
+    version="0.1.6",
     author="nand0san",
     author_email="",
     description="KlineTimestamp is a Python library designed to efficiently handle timestamps within discrete time intervals, commonly known as klines or candlesticks, often used in financial data analysis.",
@@ -32,4 +32,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    exclude_package_data={'': ['*.ipynb', '*.ipynb_checkpoints/*']},  # Exclusión de notebooks y checkpoints
 )
