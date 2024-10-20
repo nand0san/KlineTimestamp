08643d6f86aaf7ca7291ecfa5b8cfbd4060924c6
nand0san <hancaidolosdos@hotmail.com>
2024-10-20 18:59:00 +0200

    v1.0.4

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
-with open("README.md", "r", encoding="utf-8") as fh:
+# Obtener el directorio donde se encuentra setup.py
+this_directory = Path(__file__).parent
+
+# Leer README.md para el long_description
+readme_path = this_directory / "README.md"
+with open(readme_path, "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-# Leer el archivo requirements.txt para obtener las dependencias
-with open('requirements.txt') as f:
+# Leer requirements.txt para obtener las dependencias
+requirements_path = this_directory / "requirements.txt"
+with open(requirements_path, encoding="utf-8") as f:
     required_packages = f.read().splitlines()
 
 setup(
     name="kline_timestamp",  # Nombre del paquete en minúsculas
-    version="0.1.3",
+    version="0.1.4",
     author="nand0san",
     author_email="",
     description="KlineTimestamp is a Python library designed to efficiently handle timestamps within discrete time intervals, commonly known as klines or candlesticks, often used in financial data analysis.",
@@ -17,6 +24,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/nand0san/kline_timestamp",
     packages=find_packages(),
+    include_package_data=True,  # Asegura que se respete MANIFEST.in
     install_requires=required_packages,
     classifiers=[
         "Programming Language :: Python :: 3",
